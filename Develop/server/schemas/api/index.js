e3236297d9c3fb25f646b2714d2e7b990e771a50
Cody T <crthorn11@gmail.com>
2024-05-01 20:39:39 -0400

    Created my initial Type defs, changed the user and book schema to be formatted more for graphql, changed some definitions in the models

--- Develop/server/schemas/api/index.js
+++ Develop/server/schemas/api/index.js
@@ -1,6 +1,12 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+// const router = require('express').Router();
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
-module.exports = router;
+// module.exports = router;
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
+
